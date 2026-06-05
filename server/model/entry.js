7a8a80742188854c851c5abd46dfc8dd72b5a7a4
Dhriti Kohli <dhritikohli@gmail.com>
2026-06-04 20:34:47 -0500

    Add server code

---
+++ server/model/entry.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+// FIXME: Need to change this schema to match the things I wanna track/ add in the watchlist. 
+
+const schema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  competency: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+});
+
+const Entry = mongoose.model("Entry", schema);
+
+module.exports = Entry;
